88e1b267682f9ed207fbb8ab0728925ac97e2c3e
sandoval <20200300@ricaldone.edu.sv>
2024-02-26 20:13:42 -0600

    agrege una nueva carpeta de JS y agrege el js para el catalogo

---
+++ JavaScript/catalogo.js
@@ -0,0 +1,36 @@
+let catalogo = document.getElementById("catalogo");
+let seleccion = document.getElementById("seleccion");
+let imgSeleccionada = document.getElementById("img");
+let modeloSeleccionado = document.getElementById("modelo");
+let descripSeleccionada = document.getElementById("Nombre");
+let descSeleccionado = document.getElementById("Descripcion");
+
+function cargar(item){
+    quitarBordes();
+    mostrador.style.width = "60%";
+    seleccion.style.width = "40%";
+    seleccion.style.opacity = "1";
+    item.style.border = "2px solid red";
+
+    imgSeleccionada.src = item.getElementsByTagName("img")[0].src;
+
+    modeloSeleccionado.innerHTML =  item.getElementsByTagName("p")[0].innerHTML;
+
+    descSeleccionada.innerHTML = "Descripción del modelo ";
+
+    descripSeleccionado.innerHTML =  item.getElementsByTagName("span")[0].innerHTML;
+
+
+}
+function cerrar(){
+    mostrador.style.width = "100%";
+    seleccion.style.width = "0%";
+    seleccion.style.opacity = "0";
+    quitarBordes();
+}
+function quitarBordes(){
+    var items = document.getElementsByClassName("item");
+    for(i=0;i <items.length; i++){
+        items[i].style.border = "none";
+    }
+}
